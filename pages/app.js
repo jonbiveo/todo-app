b4e7e9125d053fe89f5d8d7b139572eb54572a5e
jonbiveo <logantedmonds@gmail.com>
2021-11-26 20:22:09 -0600

    index updated with blank task entry

---
+++ pages/app.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+import { ChakraProvider } from "@chakra-ui/react"
+
+function App({ Component }) {
+  return (
+    <ChakraProvider>
+      <Component />
+    </ChakraProvider>
+  )
+}
